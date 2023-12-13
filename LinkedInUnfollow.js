8afa4d6890169b833f04fc8d288002ae9bf55c8f
Rahul Mali <rahulmali31415@gmail.com>
2023-12-13 21:28:42 +0530

    Create LinkedInUnfollow.js

---
+++ LinkedInUnfollow.js
@@ -0,0 +1,32 @@
+// Navigate to My Network => Following and Followers.
+// Copy this JS snippet and paste it into console of your browser.
+// Scroll down the list of connection you are following.
+// Use with caution. This will unfollow everyone.
+// Refresh page when done.
+
+function unfollowEveryone(){
+	const spanTags = document.querySelectorAll('span');
+
+	spanTags.forEach(isFollowing)
+
+	function isFollowing(currentElement){
+		if(currentElement.outerText == 'Following'){
+			currentElement.click();
+			setTimeout(unfollowClick, 500);
+		}
+	}
+}
+
+function unfollowClick(){
+		const innerSpanTags = document.querySelectorAll('span');
+
+		innerSpanTags.forEach(unfollow)
+
+		function unfollow(currentElement){
+			if(currentElement.outerText == 'Unfollow'){
+				currentElement.click();
+			}
+		}
+	}
+	
+setInterval(unfollowEveryone, 1000);
